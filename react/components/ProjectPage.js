a7c687b994101a49edf6a0c936b2688d6276106e
Cade Krueger <Cadeak22@outlook.com>
2024-10-10 16:14:45 -0500

    Major progress on getting react components set up as well as creating the project showcase page.

---
+++ react/components/ProjectPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProjectPage = () => {
+
+    return (
+        <div className="ProjectPage">
+            Project Page - The project Page contains all info about a given project.
+            <ProjectView />
+        </div>
+    );
+}
+
+export default ProjectPage;
---
+++ react/components/ProjectView.js
@@ -0,0 +1,19 @@
+import React from 'react'; // Make sure to import React
+
+const ProjectView = () => {
+
+    const style = {
+        backgroundColor: 'red',
+        width: '80%',
+        height: '100%',
+        color: 'white'
+    }
+
+    return (
+      <div className="ProjectView">
+        Project View - The project view is in the houseeeee
+      </div>
+    );
+}
+
+export default ProjectView;
